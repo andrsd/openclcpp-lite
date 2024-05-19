8097f7834ee59acdcc2a503a7320a1a652ab0676
David Andrs <andrsd@gmail.com>
2024-05-19 10:44:40 -0600

    Test for Exception

---
+++ tests/Exception_test.cpp
@@ -0,0 +1,15 @@
+#include "gmock/gmock.h"
+#include "openclcpp-lite/openclcpp-lite.h"
+
+namespace ocl = openclcpp_lite;
+
+TEST(ExceptionTest, test)
+{
+    try {
+        throw ocl::Exception("asdf");
+        FAIL();
+    }
+    catch (ocl::Exception & e) {
+        EXPECT_STREQ(e.what(), "asdf");
+    }
+}
