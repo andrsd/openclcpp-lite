1a677c99423f44cffa2915dd69770e85682d4bf9
David Andrs <andrsd@gmail.com>
2024-05-28 21:50:28 -0600

    Wrapping more API that provide device information

--- include/openclcpp-lite/device.h
+++ include/openclcpp-lite/device.h
@@ -28,25 +28,139 @@ public:
     /// Is `true` if the device is available and `false` if the device is not available.
     bool available() const;
 
+    /// A list of built-in kernels supported by the device. An empty string is returned if no
+    /// built-in kernels are supported by the device.
+    std::vector<std::string> built_in_kernels() const;
+
     /// Is `false` if the implementation does not have a compiler available to compile the program
     /// source. Is `true` if the compiler is available. This can be `false` for the embedded
     /// platform profile only.
     bool compiler_available() const;
 
+    /// Is `true` if the device implements error correction for all accesses to compute device
+    /// memory (global and constant). Is `false` if the device does not implement such error
+    /// correction.
+    bool error_correction_support() const;
+
+    /// Returns a list of extension names (the extension names themselves do not contain any spaces)
+    /// supported by the device.
+    std::vector<std::string> extensions() const;
+
+    /// Is `true` if the OpenCL device is a little endian device and `false` otherwise.
+    bool endian_little() const;
+
     /// Size of global memory cache line in bytes
     unsigned long global_mem_cache_size() const;
+
+    /// Size of global memory cache line in bytes.
+    unsigned int global_mem_cache_line_size() const;
 
     /// Size of global device memory in bytes
     unsigned long global_mem_size() const;
 
-    /// Size of local memory arena in bytes. The minimum value is 32 KB for devices that are not of
-    /// type `CUSTOM`.
+    /// Is `true` if the device and the host have a unified memory subsystem and is `false`
+    /// otherwise.
+    bool host_unified_memory() const;
+
+    /// Is `true` if images are supported by the OpenCL device and `false` otherwise.
+    bool image_support() const;
+
+    /// Max height of 2D image in pixels. The minimum value is 8192 if `image_support` is `true`.
+    size_t image2d_max_height() const;
+
+    /// Max width of 2D image or 1D image not created from a buffer object in pixels. The minimum
+    /// value is 8192 if `image_support()` is `true`.
+    size_t image2d_max_width() const;
+
+    /// Max depth of 3D image in pixels. The minimum value is 2048 if `image_support()` is `true`.
+    size_t image3d_max_depth() const;
+
+    /// Max height of 3D image in pixels. The minimum value is 2048 if `image_support()` is `true`.
+    size_t image3d_max_height() const;
+
+    /// Max width of 3D image in pixels. The minimum value is 2048 if `image_support()` is `true`.
+    size_t image3d_max_width() const;
+
+    /// Max number of pixels for a 1D image created from a buffer object. The minimum value is 65536
+    /// if `image_support()` is `true`.
+    size_t image_max_buffer_size() const;
+
+    /// Max number of images in a 1D or 2D image array. The minimum value is 2048 if
+    /// `image_support()` is `true`
+    size_t image_max_array_size() const;
+
+    /// Is `false` if the implementation does not have a linker available. Is `true` if the linker
+    /// is available. This can be `false` for the embedded platform profile only. This must be
+    /// `true` if `compiler_available` is `true`.
+    bool linker_available() const;
+
+    /// Size of local memory arena in bytes. The minimum value is 32 KB for devices that are not
+    /// of type `CUSTOM`.
     unsigned long local_mem_size() const;
+
+    /// Maximum configured clock frequency of the device in MHz.
+    unsigned int max_clock_frequency() const;
+
+    /// The number of parallel compute units on the OpenCL device. A work-group executes on a single
+    /// compute unit. The minimum value is 1.
+    unsigned int max_compute_units() const;
+
+    /// Max number of arguments declared with the `__constant` qualifier in a kernel. The minimum
+    /// value is 8 for devices that are not of type `CUSTOM`.
+    unsigned int max_constant_args() const;
+
+    /// Max size in bytes of a constant buffer allocation. The minimum value is 64 KB for devices
+    /// that are not of type `CUSTOM`.
+    unsigned long max_constant_buffer_size() const;
+
+    /// Max size of memory object allocation in bytes. The minimum value is max (1/4th of
+    /// `global_mem_size()`, 128*1024*1024) for devices that are not of type `CUSTOM`.
+    unsigned long max_mem_alloc_size() const;
+
+    /// Max size in bytes of the arguments that can be passed to a kernel. The minimum value is 1024
+    /// for devices that are not of type `CUSTOM`. For this minimum value, only a maximum of 128
+    /// arguments can be passed to a kernel.
+    size_t max_parameter_size() const;
+
+    /// Max number of simultaneous image objects that can be read by a kernel. The minimum value is
+    /// 128 if `image_support()` is `true`.
+    unsigned int max_read_image_args() const;
+
+    /// Maximum number of samplers that can be used in a kernel. The minimum value is 16 if
+    /// `image_support()` is `true`. (Also see Sampler.)
+    unsigned int max_samplers() const;
 
     /// Maximum number of work-items in a work-group executing a kernel on a single compute unit,
     /// using the data parallel execution model. (Refer to `clEnqueueNDRangeKernel`). The minimum
     /// value is `1`.
     size_t max_work_group_size() const;
+
+    /// Maximum dimensions that specify the global and local work-item IDs used by the data parallel
+    /// execution model. (Refer to `clEnqueueNDRangeKernel`). The minimum value is 3 for devices
+    /// that are not of type `CUSTOM`.
+    unsigned int max_work_item_dimensions() const;
+
+    /// Maximum number of work-items that can be specified in each dimension of the work-group to
+    /// `clEnqueueNDRangeKernel`.
+    ///
+    /// Returns n size_t entries, where n is the value returned by the query for
+    /// `max_work_item_dimensions`.
+    ///
+    /// The minimum value is (1, 1, 1) for devices that are not of type `CUSTOM`.
+    std::vector<size_t> max_work_item_sizes() const;
+
+    /// Max number of simultaneous image objects that can be written to by a kernel. The minimum
+    /// value is 8 if `image_support` is `true`.
+    unsigned int max_write_image_args() const;
+
+    /// The minimum value is the size (in bits) of the largest OpenCL built-in data type supported
+    /// by the device (`long16` in `FULL` profile, `long16` or `int16` in `EMBEDDED` profile) for
+    /// devices that are not of type `CUSTOM`.
+    unsigned int mem_base_addr_align() const;
+
+    /// Deprecated in OpenCL 1.2. The smallest alignment in bytes which can be used for any data
+    /// type.
+    unsigned int min_data_type_align_size() const;
 
     /// Device name
     std::string name() const;
@@ -66,6 +180,10 @@ public:
     /// OpenCL 1.1.
     std::string open_cl_version() const;
 
+    /// Returns the maximum number of sub-devices that can be created when a device is partitioned.
+    /// The value returned cannot exceed `max_compute_units`.
+    unsigned int partition_max_sub_devices() const;
+
     /// OpenCL profile string. Returns the profile name supported by the device (see note). The
     /// profile name returned can be one of the following strings:
     ///
@@ -74,6 +192,9 @@ public:
     ///
     /// `EMBEDDED_PROFILE` - if the device supports the OpenCL embedded profile.
     std::string profile() const;
+
+    /// Describes the resolution of device timer. This is measured in nanoseconds.
+    size_t profiling_timer_resolution() const;
 
     /// Vendor name
     std::string vendor() const;
--- src/device.cpp
+++ src/device.cpp
@@ -17,10 +17,36 @@ Device::available() const
     return get_info<cl_bool>(CL_DEVICE_AVAILABLE);
 }
 
+std::vector<std::string>
+Device::built_in_kernels() const
+{
+    auto str = get_info<std::string>(CL_DEVICE_BUILT_IN_KERNELS);
+    return utils::split(str, ";");
+}
+
 bool
 Device::compiler_available() const
 {
     return get_info<cl_bool>(CL_DEVICE_COMPILER_AVAILABLE);
+}
+
+bool
+Device::endian_little() const
+{
+    return get_info<cl_bool>(CL_DEVICE_ENDIAN_LITTLE);
+}
+
+bool
+Device::error_correction_support() const
+{
+    return get_info<cl_bool>(CL_DEVICE_ERROR_CORRECTION_SUPPORT);
+}
+
+std::vector<std::string>
+Device::extensions() const
+{
+    auto str = get_info<std::string>(CL_DEVICE_EXTENSIONS);
+    return utils::split(str, " ");
 }
 
 unsigned long
@@ -29,10 +55,76 @@ Device::global_mem_cache_size() const
     return get_info<cl_ulong>(CL_DEVICE_GLOBAL_MEM_CACHE_SIZE);
 }
 
+unsigned int
+Device::global_mem_cache_line_size() const
+{
+    return get_info<cl_uint>(CL_DEVICE_GLOBAL_MEM_CACHELINE_SIZE);
+}
+
 unsigned long
 Device::global_mem_size() const
 {
     return get_info<cl_ulong>(CL_DEVICE_GLOBAL_MEM_SIZE);
+}
+
+bool
+Device::host_unified_memory() const
+{
+    return get_info<cl_bool>(CL_DEVICE_HOST_UNIFIED_MEMORY);
+}
+
+bool
+Device::image_support() const
+{
+    return get_info<cl_bool>(CL_DEVICE_IMAGE_SUPPORT);
+}
+
+size_t
+Device::image2d_max_height() const
+{
+    return get_info<size_t>(CL_DEVICE_IMAGE2D_MAX_HEIGHT);
+}
+
+size_t
+Device::image2d_max_width() const
+{
+    return get_info<size_t>(CL_DEVICE_IMAGE2D_MAX_WIDTH);
+}
+
+size_t
+Device::image3d_max_depth() const
+{
+    return get_info<size_t>(CL_DEVICE_IMAGE3D_MAX_DEPTH);
+}
+
+size_t
+Device::image3d_max_height() const
+{
+    return get_info<size_t>(CL_DEVICE_IMAGE3D_MAX_HEIGHT);
+}
+
+size_t
+Device::image3d_max_width() const
+{
+    return get_info<size_t>(CL_DEVICE_IMAGE3D_MAX_WIDTH);
+}
+
+size_t
+Device::image_max_buffer_size() const
+{
+    return get_info<size_t>(CL_DEVICE_IMAGE_MAX_BUFFER_SIZE);
+}
+
+size_t
+Device::image_max_array_size() const
+{
+    return get_info<size_t>(CL_DEVICE_IMAGE_MAX_ARRAY_SIZE);
+}
+
+bool
+Device::linker_available() const
+{
+    return get_info<cl_bool>(CL_DEVICE_LINKER_AVAILABLE);
 }
 
 unsigned long
@@ -41,10 +133,88 @@ Device::local_mem_size() const
     return get_info<cl_ulong>(CL_DEVICE_LOCAL_MEM_SIZE);
 }
 
+unsigned int
+Device::max_clock_frequency() const
+{
+    return get_info<cl_uint>(CL_DEVICE_MAX_CLOCK_FREQUENCY);
+}
+
+unsigned int
+Device::max_compute_units() const
+{
+    return get_info<cl_uint>(CL_DEVICE_MAX_COMPUTE_UNITS);
+}
+
+unsigned int
+Device::max_constant_args() const
+{
+    return get_info<cl_uint>(CL_DEVICE_MAX_CONSTANT_ARGS);
+}
+
+unsigned long
+Device::max_constant_buffer_size() const
+{
+    return get_info<cl_ulong>(CL_DEVICE_MAX_CONSTANT_BUFFER_SIZE);
+}
+
+unsigned long
+Device::max_mem_alloc_size() const
+{
+    return get_info<cl_ulong>(CL_DEVICE_MAX_MEM_ALLOC_SIZE);
+}
+
+size_t
+Device::max_parameter_size() const
+{
+    return get_info<size_t>(CL_DEVICE_MAX_PARAMETER_SIZE);
+}
+
+unsigned int
+Device::max_read_image_args() const
+{
+    return get_info<cl_uint>(CL_DEVICE_MAX_READ_IMAGE_ARGS);
+}
+
+unsigned int
+Device::max_samplers() const
+{
+    return get_info<cl_uint>(CL_DEVICE_MAX_SAMPLERS);
+}
+
 size_t
 Device::max_work_group_size() const
 {
     return get_info<size_t>(CL_DEVICE_MAX_WORK_GROUP_SIZE);
+}
+
+unsigned int
+Device::max_work_item_dimensions() const
+{
+    return get_info<size_t>(CL_DEVICE_MAX_WORK_ITEM_DIMENSIONS);
+}
+
+std::vector<size_t>
+Device::max_work_item_sizes() const
+{
+    return get_info<std::vector<size_t>>(CL_DEVICE_MAX_WORK_ITEM_SIZES);
+}
+
+unsigned int
+Device::max_write_image_args() const
+{
+    return get_info<cl_uint>(CL_DEVICE_MAX_WRITE_IMAGE_ARGS);
+}
+
+unsigned int
+Device::mem_base_addr_align() const
+{
+    return get_info<cl_uint>(CL_DEVICE_MEM_BASE_ADDR_ALIGN);
+}
+
+unsigned int
+Device::min_data_type_align_size() const
+{
+    return get_info<cl_uint>(CL_DEVICE_MIN_DATA_TYPE_ALIGN_SIZE);
 }
 
 std::string
@@ -59,10 +229,22 @@ Device::open_cl_version() const
     return get_info<std::string>(CL_DEVICE_OPENCL_C_VERSION);
 }
 
+unsigned int
+Device::partition_max_sub_devices() const
+{
+    return get_info<cl_uint>(CL_DEVICE_PARTITION_MAX_SUB_DEVICES);
+}
+
 std::string
 Device::profile() const
 {
     return get_info<std::string>(CL_DEVICE_PROFILE);
+}
+
+size_t
+Device::profiling_timer_resolution() const
+{
+    return get_info<size_t>(CL_DEVICE_PROFILING_TIMER_RESOLUTION);
 }
 
 std::string
