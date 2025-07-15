b2ce2031efdb9ac07be7b87ac37b484731bf4695
David Andrs <andrsd@gmail.com>
2025-07-15 06:01:58 -0600

    Use gcov we discovered via find_program

--- cmake/CodeCoverage.cmake
+++ cmake/CodeCoverage.cmake
@@ -62,6 +62,10 @@ if(OPENCLCPP_LITE_CODE_COVERAGE)
 
     elseif(CMAKE_C_COMPILER_ID MATCHES "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES "GNU")
         find_program(GCOV_PATH NAMES gcov)
+        if (NOT GCOV_PATH)
+            message(FATAL_ERROR "gcov not found!")
+        endif()
+
         find_program(LCOV_PATH lcov)
         find_program(GENHTML_PATH genhtml)
         mark_as_advanced(FORCE GCOV_PATH LCOV_PATH GENHTML_PATH)
@@ -75,7 +79,7 @@ if(OPENCLCPP_LITE_CODE_COVERAGE)
 
         add_custom_command(
             OUTPUT ${COVERAGE_INFO}
-            COMMAND ${LCOV_PATH} --capture --directory ${PROJECT_BINARY_DIR} --output-file ${COVERAGE_INFO} ${EXCLUDE_REGEX}
+            COMMAND ${LCOV_PATH} --capture --directory ${PROJECT_BINARY_DIR} --output-file ${COVERAGE_INFO}  --gcov-tool ${GCOV_PATH} ${EXCLUDE_REGEX}
         )
 
         add_custom_target(htmlcov DEPENDS ${CMAKE_BINARY_DIR}/htmlcov/index.html)
